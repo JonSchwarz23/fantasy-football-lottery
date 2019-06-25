5b7014bc008238d9ccfc95fd461c24d0f093b296
Jonathan Schwarz <JonSchwarz23@gmail.com>
2019-06-24 20:33:47 -0400

    Updates

---
+++ precache-manifest.debb0740de44b4033d952bca80e53e4a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b549f8f0f89f9db43b6a290938d52b5",
+    "url": "JonSchwarz23.github.io/fantasy-lottery-2019/index.html"
+  },
+  {
+    "revision": "ef89466f86a65ef5630c",
+    "url": "JonSchwarz23.github.io/fantasy-lottery-2019/static/js/2.dc860891.chunk.js"
+  },
+  {
+    "revision": "b06097d10bbab78d1a0c",
+    "url": "JonSchwarz23.github.io/fantasy-lottery-2019/static/js/main.df426302.chunk.js"
+  },
+  {
+    "revision": "42cb0a27fe23330fcb39",
+    "url": "JonSchwarz23.github.io/fantasy-lottery-2019/static/js/runtime~main.6cc469de.js"
+  }
+]);
